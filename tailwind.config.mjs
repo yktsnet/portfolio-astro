a35c48a7a0cb2c4a81bb65b3804592c965460738
yktsnet <ykatsu24@gmail.com>
2026-02-25 13:14:02 +0000

    improved all

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,12 @@ export default {
           green: "#addb67",
           pink: "#bae67e",
           border: "#303446",
+          accent: "#5de4c7",
+          "accent-hover": "#4ebca5",
+          muted: "#a6accd",
+        },
+        light: {
+          accent: "#d0679d",
         }
       },
       fontFamily: {
