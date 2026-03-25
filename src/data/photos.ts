562043f9bdd44a46955addc6ad22d9812454d563
yktsnet <ykatsu24@gmail.com>
2026-03-25 07:50:30 +0000

    update photo page

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -18,9 +18,9 @@ export const photoGroups: PhotoGroup[] = [
   { range: [42,  54],  location: "Thailand",  date: "2018.12" },
   { range: [55,  89],  location: "Tunisia", date:"2015. 08" },
   { range: [90,  100],  location: "Portland, US",  date: "2016.05" },
-  { range: [101,  112], location: "Japan",    date: "2024.10" },
-  { range: [113, 134], location: "Austlia, Slovenia, Italy",    date: "2024.11" },
-  { range: [135,  152],  location: "Japan",  date: "2018.12" },
+  { range: [101,  112], location: "Japan",    date: "2018.04" },
+  { range: [113, 134], location: "Austlia, Slovenia, Italy",    date: "2018.05" },
+  { range: [135,  151],  location: "Japan",  date: "2018.04" },
 ];
 
 export const photos: Photo[] = Array.from({ length: 152 }, (_, i) => {
