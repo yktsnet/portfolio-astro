6371a38760f780c9999e86dbcc3e16015ccb661f
yktsnet <ykatsu24@gmail.com>
2026-04-02 04:32:20 +0000

    add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://ykts.net',
@@ -15,5 +16,5 @@ export default defineConfig({
        external: ["node:fs", "node:path"],
     },
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()]
 });
